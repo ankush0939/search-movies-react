3a9d7e4d2d22240038d7fe209b71ec02d4d38892
Ankush Mishra <ankushmishra0939@gmail.com>
2021-08-19 12:59:46 +0530

    search and fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+import './App.css';
+import SearchForm from './Components/SearchForm';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+      <h1 className='title' id='startShow'>Movie Search</h1>
+      
+      <SearchForm />
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/SearchForm.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { useState } from "react";
+
+const SearchForm = () => {
+  const [query, setquery] = useState('');
+  const [ movies, setMovies ] = useState( [] );
+
+  const url = `https://api.themoviedb.org/3/search/movie?api_key=5dcf7f28a88be0edc01bbbde06f024ab&language=en-US&query=${query}&page=1&include_adult=false`;
+
+  const getMovies = async (e) => {
+    try {
+      e.preventDefault();
+
+      const result = await fetch( url );
+
+      const data = await result.json();
+
+      setMovies( data.results );
+
+      console.log('My Result: ', movies);
+    } catch (error) {
+        console.log('fetch error', error.message);
+    }
+  };
+
+  return (
+    <div id="content" className="section">
+      <form className="form-control" onSubmit={getMovies}>
+        <label className="form-lable">Movie Name</label>
+        <input
+          className="form-input"
+          type="text"
+          name="query"
+          required
+          placeholder="Enter the movie name"
+          onChange={(e) => setquery(e.target.value)}
+        />
+        <button className="form-button" type="submit">
+          Search
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchForm;
